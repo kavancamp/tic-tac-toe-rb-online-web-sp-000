c18e7d0f5100e5ab2269fc2d741f34ea16465a56
Keenah Vancampenhout <kvancamp@mail.com>
2020-07-21 16:16:43 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -46,7 +46,24 @@ end
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
+def turn_count(board)
+  turns = 0
+  board.each do |space|
+    if space == "X" || space == "O"
+      turns += 1
+    end
+  end
+  return turns
+end
 
+def current_player(board)
+  turns = turn_count(board)
+  if turns % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
   [0,1,2], # top
